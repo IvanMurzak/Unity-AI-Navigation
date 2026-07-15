6dd1e393ce216de4306dd5a683d5122f7186bbd2
Ivan Murzak <Ivan.D.Murzak@gmail.com>
2026-07-15 15:43:56 -0700

    AI Game Developer update

--- Installer/Assets/com.IvanMurzak/AI Navigation Installer/Installer.cs
+++ Installer/Assets/com.IvanMurzak/AI Navigation Installer/Installer.cs
@@ -19,7 +19,7 @@ namespace com.IvanMurzak.Unity.MCP.Navigation.Installer
     public static partial class Installer
     {
         public const string PackageId = "com.ivanmurzak.unity.mcp.navigation";
-        public const string Version = "1.0.11";
+        public const string Version = "1.0.12";
 
         static Installer()
         {
